8fbbb7f48b6de54d81673fb5eb347cf63bb58dd2
pandarolling <timothykurian@gmail.com>
2026-04-06 17:32:25 +0530

    main function

---
+++ shell.c
@@ -0,0 +1,17 @@
+/************************************************************************************
+* MyShell - simple unix shell.                                                      *
+* By - Timothy Kurian and Shubham Yadav                                             *
+* Description -                                                             
+************************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(){
+
+   //this loop performs all the input,output and executing of the shell
+   myshell_loop();
+
+return 0;
+}

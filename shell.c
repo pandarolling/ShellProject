705fdae6c603620950f6a6f85a2f1489fabf3ffd
pandarolling <timothykurian@gmail.com>
2026-04-12 21:40:40 +0530

    added based builtin functions and completed the splitline function

--- shell.c
+++ shell.c
@@ -9,19 +9,115 @@
 #include<string.h>
 
 
+
+
+
+
+/*
+change directory
+args[0] is "cd" args[1] is the directory
+returns 1 to continue execution
+*/
+int mysh_cd(char ** args){
+   if(args[1] == NULL){
+      fprintf(stderr, "mysh: expected arguments for \"cd\" \n");
+
+   }else{
+      if(chdir(args[1]) != 0){
+         perror("mysh");
+      }
+   }
+
+   return 1;
+
+}
+
+
+
+
+/*
+print help 
+returns 1 for continuing execution
+*/
+int mysh_help(char ** args){
+   int i ;
+   printf("MyShell program\n");
+   printf("Type program names and hit enter\n");
+   printf("Following are built in\n");
+
+   for(i = 0; i < mysh_num_builtins(); i++){
+      printf(" %s\n", builtin_str[i]);
+   }
+
+   printf("Use the man command for information on other programs.\n");
+   return 1;
+
+}
+
+
+/*
+exit function
+returns 0 to terminate
+*/
+
+int mysh_exit(char **args){
+   return 0;
+}
+
+
+
+/*
+launch a program and  wait for it to terminate\
+returns 1 to continue execution
+*/
+
+int mysh_launch(char **args){
+   pid_t pid;
+   int status;
+
+   pid = fork();
+
+   if(pid == 0){
+      //child process
+      if(execvp(args[0], args) == -1){
+         perror("mysh");
+      }
+      exit(EXIT_FAILURE);
+   }else if(pid < 0){
+      perror("mysh: error in forking" );
+   }else{
+      do{
+         waitpid(pid, &status, WUNTRACED);
+      }while(!WIFEXITED(status) && !WIFSIGNALED(status));
+   }
+
+
+   return 1;
+}
+
+
+
+
 /*
 Execute the tokens
 */
 int mysh_execute(char ** args){
-   
+   int i ;
+
+   if(args[0] ==NULL){
+      return 1;
+
+   }
+
+   for(i =0; i < mysh_num_builtins(); i++){
+      if(strcmp(args[0] , builtin_str[i]) == 0){
+         return (*builtin_func[i])(args);
+      }
+   }
+
+   return mysh_launch(args);
 }
 
-/*
-Split line into tokens
-*/
-char ** mysh_splitline(char * line){
-
-}
 
 
 /*
@@ -77,6 +173,49 @@ char * mysh_readline(){
 #endif
 }
 
+
+
+/*
+Split line into tokens
+*/
+
+#define MYSH_TOK_BUFSIZE 64;
+#define MYSH_TOK_DELIM " \t\n\r\a"
+   
+char ** mysh_splitline(char *line )
+{
+   int bufsize = MYSH_TOK_BUFSIZE;
+   int position = 0;
+   char **tokens = malloc(bufsize * sizeof(char *));
+   char * token, **tokens_backup;
+
+   if(!tokens){
+      fprintf(stderr, "mysh: allocation error\n");
+      exit(EXIT_FAILURE);
+   }
+
+   token = strtok(line, MYSH_TOK_DELIM);
+   while(token != NULL){
+      tokens[position] = token;
+      position++;
+
+      if(position>= bufsize){
+         bufsize += MYSH_TOK_BUFSIZE;
+         tokens_backup = tokens;
+         tokens = realloc(tokens, bufsize* sizeof(char *));
+         if(!tokens){
+            free(tokens_backup);
+            fprintf(stderr, "mysh: allocation error\n");
+            exit(EXIT_FAILURE);
+         }
+      }
+
+      token = strtok(NULL, MYSH_TOK_DELIM);
+   }
+
+   tokens[position] = NULL;
+   return tokens;
+}
 
 /*
    looping function
